805fc8679b77ca8ab8e278e6497514d4ed737825
Alessandro Fuschi <alessandro.fuschi.work@gmail.com>
2026-02-24 10:14:37 +0100

    added current time to the deploy log file name

--- etl/bin/deploy_sql.sh
+++ etl/bin/deploy_sql.sh
@@ -14,7 +14,7 @@ REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
 LOG_DIR="${REPO_DIR}/logs"
 mkdir -p "$LOG_DIR"
 
-LOG_FILE="${LOG_DIR}/deploy_sql_$(date +'%Y%m%d').log"
+LOG_FILE="${LOG_DIR}/deploy_sql_$(date +'%Y%m%d_%H%M%S').log"
 
 if [[ $# -lt 1 ]]; then
   echo "Usage: $0 <sql-file> [<sql-file> ...]" >&2
